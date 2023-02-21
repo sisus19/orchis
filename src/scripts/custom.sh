ddc9a5d2a21e146f300edd494dc9076dba27043e
sisus <104006944+sisus19@users.noreply.github.com>
2023-02-21 13:34:23 +0100

    update

---
+++ src/scripts/custom.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sed -i 's/#AutomaticUpdatePolicy.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf 
+systemctl enable rpm-ostreed-automatic.timer 
+systemctl enable flatpak-automatic.timer 
+systemctl unmask dconf-update.service 
+systemctl enable dconf-update.service 
+systemctl enable rpm-ostree-countme.service 
+rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo 
+rm -f /etc/yum.repos.d/_copr_phracek-PyCharm.repo 
+rm -f /etc/yum.repos.d/fedora-updates-testing.repo 
+rm -f /etc/yum.repos.d/fedora-updates-testing-modular.repo 
+rm -f /etc/yum.repos.d/google-chrome.repo 
+rm -f /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo 
+rm -f /etc/yum.repos.d/rpmfusion-nonfree-steam.repo 
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf 
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf 
+rm -rf /tmp/*
+rm -rf /var/* 
