6354dbf6be7e78a223308ea4bad3242adf49a187
sisus <104006944+sisus19@users.noreply.github.com>
2023-02-21 14:35:20 +0100

    Update custom.sh

--- src/scripts/custom.sh
+++ src/scripts/custom.sh
@@ -15,3 +15,4 @@ rm -f /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
 rm -f /etc/yum.repos.d/rpmfusion-nonfree-steam.repo 
 sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf 
 sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf 
+rpm-ostree cleanup -m
